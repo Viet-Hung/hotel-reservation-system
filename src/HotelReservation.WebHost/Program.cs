c34bf1972584eb54fd1224da6948a0d85b2159c0
Việt Hùng <nohobao@gmail.com>
2026-05-13 19:11:19 +0700

    feat: setup application layer with mediatR

--- src/HotelReservation.WebHost/Program.cs
+++ src/HotelReservation.WebHost/Program.cs
@@ -1,3 +1,4 @@
+using HotelReservation.Application;
 using HotelReservation.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 builder.Services.AddInfrastructure(builder.Configuration);
 
