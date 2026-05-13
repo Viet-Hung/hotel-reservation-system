edcb6956193cac4fc6a2ce575ab9c0f97cc318e9
Việt Hùng <nohobao@gmail.com>
2026-05-13 14:57:41 +0700

    feat: add application db context

---
+++ src/HotelReservation.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,27 @@
+using HotelReservation.Domain.Hotels.Entities;
+using HotelReservation.Domain.Reservations.Entities;
+using HotelReservation.Domain.Users.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelReservation.Infrastructure.Persistence;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Reservation> Reservations => Set<Reservation>();
+
+    public DbSet<Hotel> Hotels => Set<Hotel>();
+
+    public DbSet<User> Users => Set<User>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
